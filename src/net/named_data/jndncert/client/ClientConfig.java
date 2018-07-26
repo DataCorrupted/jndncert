3e6981379050624a1f7c947a7ae3fb4fafc3547f
Peter Rong <PeterRong96@gmail.com>
2018-07-25 17:45:47 -0700

    ClientConfig test passed.
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- src/net/named_data/jndncert/client/ClientConfig.java
+++ src/net/named_data/jndncert/client/ClientConfig.java
@@ -9,14 +9,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class ClientConfig {
 
     // TODO: Do this really need to be public?
-    public List<ClientCaItem> m_caItems;
-    public String m_localNdncertAnchor;
+    public ArrayList<ClientCaItem> m_caItems = new ArrayList<>();
+    public String m_localNdncertAnchor = "";
 
     public void load(String fileName){
         try {
@@ -24,6 +24,7 @@ public class ClientConfig {
             InputStream inputFileStream = new FileInputStream(f);
             JsonReader reader = Json.createReader(inputFileStream);
             load(reader);
+            reader.close();
         } catch (FileNotFoundException e) {
             // TODO: Try other logging method.
             System.err.println("Json File " + fileName + "not found.");
@@ -34,10 +35,9 @@ public class ClientConfig {
         m_caItems.clear();
         JsonObject main_obj = jsonReader.readObject();
         JsonArray arr = main_obj.getJsonArray("ca-list");
-        for (int idx = 0; !arr.isNull(idx); idx++){
+        for (int idx = 0; idx < arr.size(); idx++){
             JsonObject temp_obj = arr.getJsonObject(idx);
             m_caItems.add(extractCaItem(temp_obj));
-
         }
     }
 
@@ -46,19 +46,29 @@ public class ClientConfig {
     public void addNewCaItem(ClientCaItem item){ ; }
 
     public void removeCaItem(Name caName){
-        Predicate<ClientCaItem> caPredicate = p -> p.m_caName == caName;
+        Predicate<ClientCaItem> caPredicate = p -> p.m_caName.toUri().equals(caName.toUri());
         m_caItems.removeIf(caPredicate);
     }
 
-    ClientCaItem extractCaItem(JsonObject jsonObj){
+    /*
+     * @brief: Reads a string from the Json file. Returns "" if it doesn't exist.
+     *
+     */
+    private String getStringFromJsonObj(JsonObject obj, String name){
+        return obj.getJsonString(name) != null ? obj.getString(name) : "";
+    }
+
+    private ClientCaItem extractCaItem(JsonObject jsonObj){
         ClientCaItem ca_itme = new ClientCaItem();
 
-        // Don't worry if these items are not there, they are all properly initialized.
+        // If these items are not there, Json parser will throw an NullPointerException
+        // because the library is converting a nullptr to a String, which is meaningless.
         ca_itme.m_caName = new Name(jsonObj.getString("ca-prefix"));
-        ca_itme.m_caInfo = jsonObj.getString("ca-info");
-        ca_itme.m_probe = jsonObj.getString("probe");
-        ca_itme.m_isListEnabled = jsonObj.getString("is-list-enabled").equals("true");
-        ca_itme.m_targetedList = jsonObj.getString("target-list");
+        ca_itme.m_caInfo = getStringFromJsonObj(jsonObj, "ca-info");
+        ca_itme.m_probe = getStringFromJsonObj(jsonObj, "probe");
+        ca_itme.m_isListEnabled =
+                getStringFromJsonObj(jsonObj, "is-list-enabled").equals("true");
+        ca_itme.m_targetedList = getStringFromJsonObj(jsonObj, "target-list");
 
         // TODO: I am not sure how a certificate is issued here. Zhiyi please help.
         // ca_itme.m_anchor = new CertificateV2(jsonObj.getString("certificate"));
---
+++ test/net/named_data/jndncert/client/ClientConfigTest.groovy
@@ -0,0 +1,74 @@
+package net.named_data.jndncert.client
+
+import net.named_data.jndn.Name
+
+import javax.json.Json
+import javax.json.JsonObject
+
+class ClientConfigTest extends groovy.util.GroovyTestCase {
+    void testLoad() {
+        ClientConfig config = new ClientConfig();
+        config.load("res/test/client.conf.test");
+        assert config.m_caItems.size() == 2
+
+        final ClientCaItem item = config.m_caItems.get(0);
+        assert item.m_caName.toUri() == "/ndn/edu/ucla/CA"
+        assert item.m_caInfo == "UCLA's ceritificate authority, located in BH4805."
+        assert item.m_probe == "Please use your email address to apply a namespace first. UCLA email is preferred."
+        assert item.m_targetedList == "Use your email address (edu preferred) as input"
+        assert item.m_isListEnabled
+        // TODO: Test anchor here.
+//        assert item.m_anchor.getName().toUri() ==
+//                "/ndn/site1/KEY/%11%BC%22%F4c%15%FF%17/self/%FD%00%00%01Y%C8%14%D9%A5"
+
+        System.err.println("ClientConfig.load(): Passed.");
+    }
+
+    void testRemoveCaItem() {
+        ClientConfig config = new ClientConfig();
+        config.load("res/test/client.conf.test");
+
+        ClientCaItem item = new ClientCaItem();
+        item.m_caName = new Name("/test");
+        item.m_caInfo = "test";
+        item.m_probe = "test";
+        item.m_isListEnabled = false;
+
+        // Add
+        config.m_caItems.add(item);
+        assert config.m_caItems.size() == 3
+        assert config.m_caItems.get(2).m_caName.toUri() == "/test"
+
+        // Remove
+        config.removeCaItem(new Name("/test"));
+        assert config.m_caItems.size() == 2
+        assert config.m_caItems.get(1).m_caName.toUri() == "/ndn/edu/ucla/zhiyi/CA"
+
+        System.err.println("ClientConfig.removeCaItem(): Passed.");
+    }
+
+    void testExtractCaItem() {
+        JsonObject test_obj = Json.createObjectBuilder()
+            .add("ca-prefix", "prefix")
+            .add("ca-info", "info")
+            .add("target-list", "list")
+            .add("probe", "probe")
+            .add("is-list-enabled", "true")
+            .add("certificate", "cert")
+        .build();
+
+        ClientConfig config = new ClientConfig();
+        ClientCaItem ca_item = config.extractCaItem(test_obj);
+
+        assert ca_item.m_caName.toUri() == "/prefix"
+        assert ca_item.m_caInfo == "info"
+        assert ca_item.m_targetedList == "list"
+        assert ca_item.m_probe == "probe"
+        assert ca_item.m_isListEnabled
+        // TODO: Please also think of a way to do test on CertV2
+        // assert ca_item.m_anchor == "something"
+
+        System.err.println("ClientConfig.extractItem(): Passed.");
+
+    }
+}
